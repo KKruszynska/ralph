e08b60ea5fbc0e53bd8c6c87e9de56245ba7a48f
Katarzyna Kruszynska <kruszynskakat@gmail.com>
2026-05-04 16:47:57 -0700

    Finished controller unit test

--- tests/ralph/test_controller.py
+++ tests/ralph/test_controller.py
@@ -1,3 +1,10 @@
+import pytest
+import json
+import numpy as np
+from pathlib import Path
+
+import os
+
 from ralph.controller.controller import Controller
 
 # class TestControllerDicts:
@@ -221,8 +228,8 @@ class ControllerPathsTest:
     """
 
     def __init__(self,
-                 final_answers):
-        self.final_answers = final_answers
+                 expected_results):
+        self.expected_results = expected_results
 
     def test_launch_analysts(self):
         """
@@ -231,6 +238,7 @@ class ControllerPathsTest:
         event_list = [
                       'GaiaDR3_ULENS_025',
                       ]
+
         config = {
             'python_compiler': 'python',
             'group_processing_limit': 1,
@@ -248,13 +256,80 @@ class ControllerPathsTest:
         controller = Controller(event_list, config_dict=config)
         controller.launch_analysts()
 
+        # Check if expected files exist
+        # Controller log
+        controller_log_path = 'tests/ralph/data/output/controller_launch/'
+        output = Path(controller_log_path + 'controller.log')
+        assert output.exists() is True
+        assert output.is_file() is True
+
+        # Analyst output files
+        analyst_home = 'tests/ralph/data/input/controller/'
+        for event in event_list:
+            analyst_path = analyst_home + event
+
+            output = Path(analyst_path)
+            assert output.exists() is True
+            assert output.is_dir() is True
+
+            output = Path(analyst_path + '/fit_results.json')
+            assert output.exists() is True
+            assert output.is_file() is True
+
+            output = Path(analyst_path + '/fit_stats.txt')
+            assert output.exists() is True
+            assert output.is_file() is True
+
+            output = Path(analyst_path + '/' + event + '_analyst.log')
+            assert output.exists() is True
+            assert output.is_file() is True
+
+            model_plots = [
+                'PSPL_no_blend_no_piE',
+                'PSPL_blend_no_piE',
+                'PSPL_blend_piE',
+            ]
+
+            for file_path in model_plots:
+                output = Path(analyst_path + '/' + file_path + '.html')
+                assert output.exists() is True
+                assert output.is_file() is True
+
+            bands = [
+                '_CMD_Gaia_DR3_Gaia_G',
+                '_CMD_Gaia_DR3_Gaia_BP',
+                '_CMD_Gaia_DR3_Gaia_RP',
+            ]
+            for model in model_plots:
+                for band in bands:
+                    output = Path(analyst_path + '/' + event + '_' + model + band + '.html')
+                    assert output.exists() is True
+                    assert output.is_file() is True
+
+            expected_result_path = self.expected_results[event]
+            with open(expected_result_path, 'r') as file:
+                expected_fit_result = json.load(file)
+
+            with open(analyst_path + '/fit_results.json', 'r') as file:
+                received_fit_result = json.load(file)
+
+            for model in expected_fit_result:
+                model_result = received_fit_result[model]
+                expected_result = expected_fit_result[model]
+                for key in expected_result:
+                    expected = float(expected_result[key])
+                    received = float(model_result[key])
+                    if not np.isnan(expected):
+                        assert pytest.approx(received, 2) == pytest.approx(expected, 2)
+
+
 class ControllerPathsOngoingTest:
     """
     Tests to check if controller works fine for ongoing events.
     """
     def __init__(self,
-                 final_answers):
-        self.final_answers = final_answers
+                 expected_results):
+        self.expected_results = expected_results
 
     def test_launch_analysts(self):
         """
@@ -264,11 +339,14 @@ class ControllerPathsOngoingTest:
         # event_list = ['Gaia24amo', 'Gaia24cbz', 'AT2024kwu', 'GaiaDR3_ULENS_018',
         #               'GaiaDR3_ULENS_025']
 
-        event_list = ['AT2024kwu', 'GaiaDR3_ULENS_018']
+        event_list = [
+            'AT2024kwu',
+            'GaiaDR3_ULENS_018'
+        ]
 
         config = {
             'python_compiler': 'python',
-            'group_processing_limit': 2,
+            'group_processing_limit': 1,
             'config_type': 'yaml',
             'events_path':
                 'tests/ralph/data/input/controller/',
@@ -283,64 +361,120 @@ class ControllerPathsOngoingTest:
         controller = Controller(event_list, config_dict=config)
         controller.launch_analysts()
 
-        # # Check if expected files exist
-        # output = Path(analyst_path + 'fit_results.json')
-        # assert output.exists() is True
-        # assert output.is_file() is True
-        #
-        # output = Path(analyst_path + 'fit_stats.txt')
-        # assert output.exists() is True
-        # assert output.is_file() is True
-        #
-        # final_files = self.scenario.get('final_files')
-        # for element in final_files:
-        #     if element == 'event_folder':
-        #         output = Path(analyst_path)
-        #         assert output.exists() is True
-        #         assert output.is_dir() is True
-        #
-        #     if element == 'analyst_log':
-        #         output = Path(analyst_path + final_files[element])
-        #         assert output.exists() is True
-        #         assert output.is_file() is True
-        #
-        #     if element == 'model_plots':
-        #         for file_path in final_files[element]:
-        #             output = Path(analyst_path + file_path)
-        #             assert output.exists() is True
-        #             assert output.is_file() is True
-        #
-        #     if element == 'cmd_plots':
-        #         for file_path in final_files[element]:
-        #             output = Path(analyst_path + file_path)
-        #             assert output.exists() is True
-        #             assert output.is_file() is True
-        #
-        # with open(self.scenario.get('fit_result'), 'r') as file:
-        #     expected_fit_result = json.load(file)
-        #
-        # with open(analyst_path + 'fit_results.json', 'r') as file:
-        #     received_fit_result = json.load(file)
-        #
-        # for model in expected_fit_result:
-        #     model_result = received_fit_result[model]
-        #     expected_result = expected_fit_result[model]
-        #     for key in expected_result:
-        #         expected = float(expected_result[key])
-        #         received = float(model_result[key])
-        #         print(model, key, expected, received)
-        #         if not np.isnan(expected):
-        #             assert pytest.approx(received, 2) == pytest.approx(expected, 2)
+        # Check if expected files exist
+        # Controller log
+        controller_log_path = 'tests/ralph/data/output/controller_analysts/'
+        output = Path(controller_log_path + 'controller.log')
+        assert output.exists() is True
+        assert output.is_file() is True
+
+        # Analyst output files
+        analyst_home = 'tests/ralph/data/input/controller/'
+        for event in event_list:
+            analyst_path = analyst_home + event
+
+            output = Path(analyst_path)
+            assert output.exists() is True
+            assert output.is_dir() is True
+
+            output = Path(analyst_path + '/fit_results.json')
+            assert output.exists() is True
+            assert output.is_file() is True
+
+            output = Path(analyst_path + '/fit_stats.txt')
+            assert output.exists() is True
+            assert output.is_file() is True
+
+            output = Path(analyst_path + '/' + event + '_analyst.log')
+            assert output.exists() is True
+            assert output.is_file() is True
+
+            model_plots = [
+                'PSPL_no_blend_no_piE',
+                'PSPL_blend_no_piE',
+                'PSPL_blend_piE',
+            ]
+
+            for file_path in model_plots:
+                output = Path(analyst_path + '/' + file_path + '.html')
+                assert output.exists() is True
+                assert output.is_file() is True
+
+            expected_result_path = self.expected_results[event]
+            with open(expected_result_path, 'r') as file:
+                expected_fit_result = json.load(file)
+
+            with open(analyst_path + '/fit_results.json', 'r') as file:
+                received_fit_result = json.load(file)
+
+            for model in expected_fit_result:
+                model_result = received_fit_result[model]
+                expected_result = expected_fit_result[model]
+                for key in expected_result:
+                    expected = float(expected_result[key])
+                    received = float(model_result[key])
+                    if not np.isnan(expected):
+                        assert pytest.approx(received, 2) == pytest.approx(expected, 2)
 
 def test_run():
     """
     Run all tests.
     """
 
-    final_answers = []
+    expected_fit_results = {
+        'GaiaDR3_ULENS_025' : 'tests/ralph/data/input/test_results/gdr3_ulens_025_fit_results.json',
+        'AT2024kwu' : 'tests/ralph/data/input/test_results/at2024kwu_fit_results.json',
+        'GaiaDR3_ULENS_018' : 'tests/ralph/data/input/test_results/gdr3_ulens_018_fit_results.json',
+    }
 
-    test = ControllerPathsTest
-    test.test_launch_analysts(final_answers)
+    test = ControllerPathsTest(expected_fit_results)
+    test.test_launch_analysts()
 
-    test = ControllerPathsOngoingTest
-    test.test_launch_analysts(final_answers)
+    test = ControllerPathsOngoingTest(expected_fit_results)
+    test.test_launch_analysts()
+
+    controller_log_path = [
+        'tests/ralph/data/output/controller_launch/',
+        'tests/ralph/data/output/controller_analysts/',
+    ]
+    for controller_path in controller_log_path:
+        output = Path(controller_path + 'controller.log')
+        if output.exists():
+            os.remove(output)
+    analyst_home = 'tests/ralph/data/input/controller/'
+    for event in expected_fit_results:
+        analyst_path = analyst_home + event
+
+        output = Path(analyst_path + '/fit_results.json')
+        if output.exists():
+            os.remove(output)
+
+        output = Path(analyst_path + '/fit_stats.txt')
+        if output.exists():
+            os.remove(output)
+
+        output = Path(analyst_path + '/' + event + '_analyst.log')
+        if output.exists():
+            os.remove(output)
+
+        model_plots = [
+            'PSPL_no_blend_no_piE',
+            'PSPL_blend_no_piE',
+            'PSPL_blend_piE',
+        ]
+
+        for file_path in model_plots:
+            output = Path(analyst_path + '/' + file_path + '.html')
+            if output.exists():
+                os.remove(output)
+
+        bands = [
+            '_CMD_Gaia_DR3_Gaia_G',
+            '_CMD_Gaia_DR3_Gaia_BP',
+            '_CMD_Gaia_DR3_Gaia_RP',
+        ]
+        for model in model_plots:
+            for band in bands:
+                output = Path(analyst_path + '/' + event + '_' + model + band + '.html')
+                if output.exists():
+                    os.remove(output)
