331ed0cb9c381f7776f9428c666484fa51520d5c
Kasia <kruszynskakat@gmail.com>
2026-05-14 16:32:51 -0700

    Revert "Updates to Base analyst class"
    This reverts commit 9926b66073dc3f450770861f51469aeefcad264d.

--- src/ralph/analyst/analyst.py
+++ src/ralph/analyst/analyst.py
@@ -1,33 +1,17 @@
 import yaml
-import json
 
-class BaseAnalyst:
+
+class Analyst:
     """
-    This is a base Analyst class.
+    This is a class that analyzes one event.
     This class creates bare bones of other analysts and contains elements they all share.
 
-    :param event_name: A name of the analyzed event.
-    :type event_name: str
-
-    :param analyst_path: A path to the folder where the input and output files are saved.
-    :type analyst_path: str
-
-    :param config_dict: A dictionary with Base Analyst configuration.
-    :type config_dict: dict, optional
-
-    :param config_path: The path to the configuration file of the Base Analyst.
-    :type config_path: str, optional
-
-    Notes on configuration:
-    ------------------------------
-    The configuration dictionary has to contain the following keywords:
-
-    * `event_name`: str
-        Event name.
-    * `ra`: float
-        Right Ascension of the event in degrees.
-    * `dec`: float
-        Declination of the event in degrees.
+    :param event_name: str, name of the analyzed event
+    :param ra: float, Right Ascention of the analyzed event
+    :param dec: float, declination of the analyzed event
+    :param analyst_path: str, path to the folder where the outputs are saved
+    :param config_dict: dictionary, optional, dictionary with Analyst configuration
+    :param config_path: str, optional, path to the YAML configuration file of the Analyst
     """
 
     def __init__(self, event_name, analyst_path, config_dict=None, config_path=None):
@@ -35,40 +19,28 @@ class BaseAnalyst:
         self.event_name = self.update_names_paths(event_name)
         self.analyst_path = self.update_names_paths(analyst_path)
 
-        if (config_path is not None) or (config_dict is not None):
-            self.parse_config(config_path=config_path, config_dict=config_dict)
+        self.config = {}
+        if config_dict is not None:
+            # READ config_dict
+            self.config = config_dict
+        elif config_path is not None:
+            # read config path
+            self.parse_config(config_path)
         else:
             raise UnboundLocalError(
                     "The Analyst requires a configuration file or a configuration dictionary"
                 )
 
-    def parse_config(self, config_path=None, config_dict=None):
+    def parse_config(self, config_path):
         """
-        Either parses the file or a dictionary with configuration and
-        adds it into the internal dictionary of the Base Analyst.
+        Parse YAML file with configuration, turn it into a dictionary and add it to self.
 
-        :param config_path: A path with a YAML or JSON file containing
-            the configuration needed for the Base Analyst.
-        :type config_path: str
-
-        :param config_dict: A dictionary containing configuration for the Base Analyst.
-        :type config_dict: dict
+        :param config_path: str, path with YAML file containing Analyst configuration.
         """
 
         try:
-            if config_path is not None:
-                file_format = config_path.split(".")[-1]
-
-                if file_format == "yaml":
-                    with open(config_path, "r") as file:
-                        event_config = yaml.safe_load(file)
-                elif file_format == "json":
-                    with open(config_path, "r") as file:
-                        event_config = json.load(file)
-                        file.close()
-
-            elif config_dict is not None:
-                event_config = config_dict
+            with open(config_path, "r") as file:
+                event_config = yaml.safe_load(file)
 
             self.config["event_name"] = event_config.get("event_name")
             self.config["ra"] = float(event_config.get("ra"))
@@ -79,14 +51,10 @@ class BaseAnalyst:
 
     def update_names_paths(self, name):
         """
-        Swaps minuses and blank spaces in name to underscores, to avoid problems
-        for some operating systems.
+        This function swaps minuses in name to underscores, to avoid problems for some operating systems.
 
-        :param name: A name or path to be updated.
-        :type name: str
-
-        :return: An updated name or path without unsafe characters.
-        :rtype: str
+        :param name: str, name or path to be updated
+        :return: str, updated name without minuses
         """
 
         updated_name = name.replace(" ", "_").replace("-", "_")
